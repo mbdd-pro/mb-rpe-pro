e35f3b2787d2b5deb9391ef24e283587b63b8ec6
mbdd-pro <archivosmatiburrieza@gmail.com>
2026-06-17 19:32:37 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mb-rpe-pro-v1-006';
+const CACHE_NAME = 'mb-rpe-pro-v1-007';
 const APP_SHELL = [
   './','index.html','manifest.json',
   'css/base.css','css/themes.css','css/layout.css','css/components.css',
