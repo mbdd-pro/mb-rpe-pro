a950a03b1054a05c31863cac53a0ae2a99042335
mbdd-pro <archivosmatiburrieza@gmail.com>
2026-06-18 08:25:08 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   APP_NAME: 'MB RPE Pro',
- const CONFIG = {API_URL: "https://mb-rpe-pro-api.archivosmatiburrieza.workers.dev/",...};
+  API_URL: "https://mb-rpe-pro-api.archivosmatiburrieza.workers.dev/",
   VERSION: '1.0.7',
   DEFAULT_THEME: 'black',
   BRAND_BY: 'By Pancko'
