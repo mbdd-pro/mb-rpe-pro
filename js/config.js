3fae69483a6d3e080d208e7a38e30378ff609e9d
mbdd-pro <archivosmatiburrieza@gmail.com>
2026-06-18 17:34:17 -0300

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
   APP_NAME: 'MB RPE Pro',
   API_URL: 'https://mb-rpe-pro-api.archivosmatiburrieza.workers.dev/',
-  VERSION: '1.0.12',
+  VERSION: '1.0.12.1',
   DEFAULT_THEME: 'black',
   BRAND_BY: 'By Pancko'
 };
