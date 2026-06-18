70d554031ecff01e7768f396a1202c27f244f3bc
mbdd-pro <archivosmatiburrieza@gmail.com>
2026-06-18 08:18:39 -0300

    Change API_URL to new endpoint
    Updated API_URL to point to the new endpoint.

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   APP_NAME: 'MB RPE Pro',
-  API_URL: 'https://script.google.com/macros/s/AKfycbxjw6m1Cz7AX4l8ZxWCws0_tabGGI96Uf3YQ04dKEVZfyiO_7ihmns-jbRykKFTha-Jnw/exec',
+ const CONFIG = {API_URL: "https://mb-rpe-pro-api.archivosmatiburrieza.workers.dev/",...};
   VERSION: '1.0.7',
   DEFAULT_THEME: 'black',
   BRAND_BY: 'By Pancko'
